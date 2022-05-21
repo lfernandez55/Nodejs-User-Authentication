e4dc2e8b201fc9d3a7edbb9fb0db3f5f1926abeb
lfernandez55 <lfernandez@weber.edu>
2022-05-21 15:25:27 -0600

    Working. . . .

--- server.js
+++ server.js
@@ -6,35 +6,48 @@ app.use(express.json())
 
 const users = []
 
-app.get('/users', (req, res) => {
-  res.json(users)
+app.get('/ddd', (req, res) => {
+    console.log("DEBUG")
+    res.json("DDDDD")
 })
 
+
+app.get('/users', (req, res) => {
+    console.log("DEBUG")
+    res.json(users)
+})
+
+// app.post('/users', (req, res) => {
+//     console.log("DEBUGgggg")
+//     res.json("dork")
+// })
+
+
 app.post('/users', async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    const user = { name: req.body.name, password: hashedPassword }
-    users.push(user)
-    res.status(201).send()
-  } catch {
-    res.status(500).send()
-  }
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = { name: req.body.name, password: hashedPassword }
+        users.push(user)
+        res.status(201).send()
+    } catch {
+        res.status(500).send()
+    }
 })
 
 app.post('/users/login', async (req, res) => {
-  const user = users.find(user => user.name === req.body.name)
-  if (user == null) {
-    return res.status(400).send('Cannot find user')
-  }
-  try {
-    if(await bcrypt.compare(req.body.password, user.password)) {
-      res.send('Success')
-    } else {
-      res.send('Not Allowed')
+    const user = users.find(user => user.name === req.body.name)
+    if (user == null) {
+        return res.status(400).send('Cannot find user')
     }
-  } catch {
-    res.status(500).send()
-  }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Success')
+        } else {
+            res.send('Not Allowed')
+        }
+    } catch {
+        res.status(500).send()
+    }
 })
 
 app.listen(3000)
